d4651b9b3a3a767829bdffddfefba408173146b4
samLynux <samuelbisnes@gmail.com>
2022-03-04 16:20:33 +0700

    4-5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventController } from './event.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'nest-events',
+    })
+  ],
   controllers: [AppController, EventController],
   providers: [AppService],
 })
