2e5575915997746262d1e6898f37a9680f329065
samLynux <samuelbisnes@gmail.com>
2022-03-08 14:00:19 +0700

    9-6

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 
@@ -15,5 +15,11 @@ export class AuthController{
             userId : request.user.id,
             token: this.authService.GetTokenUser(request.user)
         }
+    }
+
+    @Get('profile')
+    @UseGuards(AuthGuard('jwt'))
+    async getProfile(@Request() request){
+        return request.user;
     }
 }
