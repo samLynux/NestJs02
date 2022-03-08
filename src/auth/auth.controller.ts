939683fa5c4b39d5d1c7ed0417b0e8ac7198aa1a
samLynux <samuelbisnes@gmail.com>
2022-03-08 13:38:47 +0700

    9-5

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController{
+    constructor(
+        private readonly authService: AuthService
+    ){}
     
     @Post('login')
     @UseGuards(AuthGuard('local'))
     async login(@Request() request){
         return {
             userId : request.user.id,
-            token: "the token go here"
+            token: this.authService.GetTokenUser(request.user)
         }
     }
 }
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { User } from "./user.entity";
+
+@Injectable()
+export class AuthService{
+    constructor(
+        private readonly jwtService:JwtService
+    ){}
+
+
+    public GetTokenUser(user:User): string {
+        return this.jwtService.sign({
+            username:user.username,
+            sub: user.id
+        });
+    }
+}
