7193d1e84a3b16d19e33debcf374dac12629fce4
samLynux <samuelbisnes@gmail.com>
2022-03-08 13:19:19 +0700

    9-3

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Controller('auth')
+export class AuthController{
+    
+    @Post('login')
+    @UseGuards(AuthGuard('local'))
+    async login(@Request() request){
+        return {
+            userId : request.user.id,
+            token: "the token go here"
+        }
+    }
+}
